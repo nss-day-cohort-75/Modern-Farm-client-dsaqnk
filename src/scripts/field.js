035dd2abb862904e1dd001c967d7b150f63c5f9d
Tanay Parekh <tanay.parekh@gmail.com>
2025-01-22 15:44:12 -0600

    completed tilling the field

---
+++ src/scripts/field.js
@@ -0,0 +1,16 @@
+const storePlants = [];
+
+export const addPlant = (seedObject) => {
+  if (Array.isArray(seedObject)) {
+    for (let item of seedObject) {
+      storePlants.push(item);
+    }
+  } else {
+    storePlants.push(seedObject);
+  }
+  return storePlants;
+};
+
+export const usePlants = () => {
+  return structuredClone(storePlants);
+};
